a045b4943a0fa5aea7de541bdd2b7b8bb016658d
Dan Ibragimov <dreamka1991@gmail.com>
2025-01-03 20:08:58 +0500

    Initial commit

---
+++ src/main/java/dan/hw/short_links/ShortLinksApplication.java
@@ -0,0 +1,13 @@
+package dan.hw.short_links;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortLinksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortLinksApplication.class, args);
+	}
+
+}
